efd8dc0d58cbe0282d7f601338dc48675938af81
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2017-02-07 22:46:50 -0800

    Data fetching done

--- public/js/angular/mainApp.js
+++ public/js/angular/mainApp.js
@@ -1,36 +1,62 @@
-var mainApp = angular.module('mainApp', ['ngRoute']);
+var mainApp = angular.module('mainApp', []);
 
-mainApp.config(function($routeProvider){
 
-  $routeProvider
-    .when('/search',{
-      templateUrl:'/doSearch'
-    });
-
-});
-
-mainApp.controller('mainCtrl', function mainCtrl($scope, $http) {
-
+mainApp.controller('mainCtrl', function($scope, $http, $templateCache) {
   $scope.searchString = "";
+  $scope.isError = false;
+  var clickToOpen = true;
+  $scope.clickClose = function(){
+    closeNav();
+  }
+  $scope.clickSearch = function(){
+    if (clickToOpen) {
+      clickToOpen = false;
+      openNav();
+    }else {
+      getDataFromSpotify($scope.searchString);
+      closeNav();
+    }
+  }
+
+  var getDataFromSpotify = function(searchString){
+    $scope.url = 'https://api.spotify.com/v1/search?q=' + searchString + '&type=track&limit=50';
+    $http(
+      {
+        method: 'GET',
+        url: $scope.url,
+        cache: $templateCache
+      }
+    ).then(function(response) {
+        console.log(response.status);
+        if(response.status == 200){
+          var data = response.data;
+          $scope.tracks = data.tracks.items;
+          console.log($scope.tracks);
+        }else {
+          console.log(response.status);
+          $scope.isError = true;
+        }
+
+      }, function(response) {
+        console.log(response.status);
+        $scope.isError = true;
+    });
+  }
 
 
   /* Open when someone clicks on the span element */
-  $scope.openNav = function() {
+  var openNav = function() {
       document.getElementById("myNav").style.width = "100%";
       document.getElementById("search-box").style.display = "block";
       document.getElementById("closebtn").style.display = "block";
   }
 
   /* Close when someone clicks on the "x" symbol inside the overlay */
-  $scope.closeNav = function() {
+  var closeNav = function() {
+      clickToOpen = true;
       document.getElementById("myNav").style.width = "8%";
       document.getElementById("search-box").style.display = "none";
       document.getElementById("closebtn").style.display = "none";
-  }
-
-
-  $scope.doSearch = function(){
-    console.log($scope.searchString);
   }
 
 });
